8e2b3c34dd1733be10634a3cfb4e42a99b30ea64
Woutsthew <nekita351@gmail.com>
2022-02-02 09:59:06 +0400

    дополнил список CommandType

--- NetTCPSocket/CommandRequestType.cs
+++ NetTCPSocket/CommandRequestType.cs
@@ -3,15 +3,32 @@ namespace NetTCPSocket
 {
     public class CommandType
     {
-        public const string CONNECT = "connect";
         public const string AUTHORIZATION = "authorization";
         public const string TABLE = "table";
         public const string SWITCHES = "switches";
         public const string MYSQL = "mysql";
         public const string USERSMYSQL = "usersmysql";
+        public const string ACCESS = "access";
 
+        public const string ACCEPT = "accept";
         public const string ACCEPTED = "accepted";
         public const string DENIED = "denied";
+        public const string REJECT = "reject";
+        public const string REJECTED = "rejected";
+
+        public const string CONNECT = "connect";
+        public const string CONNECTED = "connected";
+        public const string DISCONNECT = "disconnect";
+        public const string DISCONNECTED = "disconnected";
+
+        public const string SUCCESS = "success";
+        public const string SUCCESSED = "successed";
+
+        public const string DONE = "done";
+        public const string DONED = "doned";
+
+        public const string ERROR = "error";
+        public const string WRONG = "wrong";
     }
 
     public class RequestType
@@ -23,7 +40,7 @@ namespace NetTCPSocket
         public const string DELETE = "delete";
     }
 
-    public class CommandMessage
+    internal class CommandMessage
     {
         protected internal const string EndMessage = "<EOF>";
         protected internal const string DisconnectMessage = "<DCT>";
